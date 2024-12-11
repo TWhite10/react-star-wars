8492146a6d3c6148af99a0e0be249dcb25f5a4a0
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-12-11 16:30:37 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { getAllStarships } from './services/sw-api'
+import StarshipCard from './components/StarShipCard'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [starships, setStarships] = useState(null)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const getStarships = async () => {
+            const data = await getAllStarships()
+            setStarships(data)
+        }
+        getStarships()
+    }, [])
+
+   
+    const loaded = () => {
+        return (
+            <div className="App">
+                {starships.map((starship) => (
+                    <StarshipCard starship={starship} />
+                ))}
+            </div>
+        )
+    }
+
+ 
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+
+    return starships ? loaded() : loading()
 }
-
-export default App
---
+++ src/components/StarShipCard.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function StarshipCard({ starship }) {
+    return (
+        <div>
+            <h1>{starship.name}</h1>
+        </div>
+    );
+}
